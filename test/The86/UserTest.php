326c5c9f8279474447b32eeb3191a223f207fd47
Paul Annesley <paul@annesley.cc>
2012-08-14 15:13:53 +1000

    UserTest::testSaveToUpdate() asserts updated_at.

--- test/The86/UserTest.php
+++ test/The86/UserTest.php
@@ -33,13 +33,19 @@ class UserTest extends \PHPUnit_Framework_TestCase
 
 	public function testSaveToUpdate()
 	{
+		$response = array(
+			'id' => 8,
+			'updated_at' => '2012-08-14T00:00:00Z',
+		);
+
 		$this->http->expects($this->once())
 			->method("patch")
 			->with("/api/v1/users/8")
-			->will($this->returnValue(array('id' => 8)));
+			->will($this->returnValue($response));
 
 		$this->user->id = 8;
 		$this->user->save();
 		$this->assertEquals(8, $this->user->id);
+		$this->assertEquals('2012-08-14T00:00:00Z', $this->user->updated_at);
 	}
 }
